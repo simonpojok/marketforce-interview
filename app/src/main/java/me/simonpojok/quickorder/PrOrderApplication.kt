25e01fb2cfbf6bb2e4ffaf7950fcd50b1a15869e
simonpojok <simonpojok@gmail.com>
2022-03-30 20:18:05 +0300

    added workersa

--- app/src/main/java/me/simonpojok/quickorder/PrOrderApplication.kt
+++ app/src/main/java/me/simonpojok/quickorder/PrOrderApplication.kt
@@ -1,8 +1,18 @@
 package me.simonpojok.quickorder
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class PrOrderApplication : Application() {
+class PrOrderApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
--- app/src/main/java/me/simonpojok/quickorder/background/OrderSyncWorker.kt
+++ app/src/main/java/me/simonpojok/quickorder/background/OrderSyncWorker.kt
@@ -2,13 +2,21 @@ package me.simonpojok.quickorder.background
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import me.simonpojok.domain.usecase.SyncOrderUseCase
 import me.simonpojok.domain.usecase.UpdateOrderUseCase
 import me.simonpojok.domain.usecase.base.GetOrdersSyncStatusUseCase
+import java.util.concurrent.TimeUnit
+
+private const val WORKER_REPEAT_INTERVAL: Long = 40
+private val WORKER_TIME_UNIT = TimeUnit.MINUTES
 
 @HiltWorker
 class OrderSyncWorker @AssistedInject constructor(
@@ -22,8 +30,24 @@ class OrderSyncWorker @AssistedInject constructor(
         val orders = getOrdersSyncStatusUseCase.buildUseCase(false)
         orders.forEach { order ->
             val response = syncOrderUseCase.buildUseCase(order)
+            updateOrderUseCase.buildUseCase(response)
 
         }
         return Result.success()
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+            val workRequest = PeriodicWorkRequest.Builder(
+                OrderSyncWorker::class.java,
+                WORKER_REPEAT_INTERVAL,
+                WORKER_TIME_UNIT
+            ).setConstraints(constraints)
+                .build()
+            WorkManager.getInstance(context).enqueue(workRequest)
+        }
     }
 }
