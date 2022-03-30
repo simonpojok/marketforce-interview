516b3a1e0eba5b9a3c724464b27fa1a18eb69ceb
simonpojok <simonpojok@gmail.com>
2022-03-30 22:51:18 +0300

    added dependancies

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+    //app level
+    const val gradle = "4.0.1"
+    const val kotlin = "1.4.0"
+
+    //libs
+    val coreKtx = "1.2.0"
+    val appcompat = "1.3.0-alpha01"
+    val constraintLayout = "2.0.0-beta8"
+
+    //test
+    val junit = "4.12"
+    val extJunit = "1.1.1"
+    val espresso = "3.2.0"
+}
